2f7fdaee5d838ee923b529e5216d4373e377d511
Artem Arshaniy <artem.arshaniy108@gmail.com>
2016-12-05 13:46:45 +0200

    commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,34 +1,17 @@
-require('grunt-contrib-concat')(grunt);
+require('grunt-contrib-concat')();
 
 module.exports = function(grunt) {
 
 
 grunt.initConfig({
-concat: {
-options: {
-separator: ';'
-},
-dist: {
-src: ['js/*.js'],
-dest: 'js/script.main.js'
-}
-},
-uglify: {
-dist: {
-src: ['js/script.main.js'],
-dest: 'js/script.main.min.js'
-}
-},
-cssmin: {
-options: {
-shorthandCompacting: false,
-roundingPrecision: -1
-},
-target: {
-files: {
-'css/dist/style.min.css': ['css/src/.css']
-}
-}
+  concat: {
+    options: {
+      separator: ';'
+  },
+  dist: {
+    src: ['js/*.js'],
+      dest: 'js/script.main.js'
+  }
 }
 });
 
